a7c6b4269a0777aac9e11bb5ca7571f0b080dc65
Siddharth <goraisiddharth@gmail.com>
2024-09-07 23:50:17 +0530

    First Commit 1.6

--- app/src/main/java/com/iitism/concetto_24/adapter/CoreTeamAdapter.kt
+++ app/src/main/java/com/iitism/concetto_24/adapter/CoreTeamAdapter.kt
@@ -13,7 +13,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
-import com.iitism.concetto_24.Data.CoreTeamDataModel
+import com.iitism.concetto_24.data.CoreTeamDataModel
 import com.iitism.concetto_24.R
 
 
