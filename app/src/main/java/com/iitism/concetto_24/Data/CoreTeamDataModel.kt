ae9653656b7f68ecd014e7fd1edc337fa2ff1899
SKS <sks0202005@gmail.com>
2024-10-08 17:16:53 +0530

    Core Team Fragment Completed

--- app/src/main/java/com/iitism/concetto_24/Data/CoreTeamDataModel.kt
+++ app/src/main/java/com/iitism/concetto_24/Data/CoreTeamDataModel.kt
@@ -1,11 +1,10 @@
 package com.iitism.concetto_24.Data
 
 data class CoreTeamDataModel (
-    var image: String,
     var name:String,
-    var position: String,
+    var instagramUrl : String?,
+    var linkedinUrl : String?,
     var team: String,
-    var linkedin_url : String,
-    var insta_url : String,
-    var imgDrive:String
+    var position: String,
+    var image: String?
 )
